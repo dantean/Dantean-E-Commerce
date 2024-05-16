74321ce397b98fd62a8dfd913a0bdabcebeab654
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-16 18:58:41 -0400

    updated category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,23 +1,37 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag, Category } = require('../../models');
 
-// The `/category` endpoint
+
+// http://localhost:3001/api/categories/
+
 router.get('/', async (req, res) => {
-  try {
-const categoryData = await Category.findAll({
-  include: [Product]
-}) 
-res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
+  try{
+      const categoryData = await Category.findAll({
+        include : [{model : Product}]
+      })
+      res.status(200).json(categoryData)
+  }catch (err){
+                res.status(500).json(err)
   }
 });
 
 
+// http://localhost:3001/api/categories/1
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+        const categoryData = await Category.findByPk(req.params.id, {
+          include :[{model : Product}]
+        })
+
+        if(!categoryData) {
+          res.status(404).json({message : 'No category found with that id!'})
+          return
+        }
+        res.status(200).json(categoryData)
+  } catch (err) {
+                  res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
