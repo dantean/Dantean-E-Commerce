a47ab26960607e8084fced404975a9f61c059e85
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-16 18:17:50 -0400

    updated routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
-// The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// The `/category` endpoint
+router.get('/', async (req, res) => {
+  try {
+const categoryData = await Category.findAll({
+  include: [Product]
+}) 
+res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
